ab7defc055b8e263d869612f30f0c3f8d26586d7
nitishsurana <surana.n@husky.neu.edu>
2016-03-04 19:39:36 -0500

    Admin can add user.

--- public/project/views/admin/admin.controller.js
+++ public/project/views/admin/admin.controller.js
@@ -6,8 +6,42 @@
         .module("PortfolioManager")
         .controller("AdminController", AdminController);
 
-    function AdminController($scope){
-        $scope.admin = "ADMIN";
-        console.log("admin");
+    function AdminController($scope, UserService){
+        $scope.addUser= addUser;
+        $scope.updateUser= updateUser;
+        $scope.deleteUser= deleteUser;
+        $scope.selectUser= selectUser;
+        $scope.updateUser = updateUser;
+        $scope.deleteUser = deleteUser;
+        $scope.selectUser = selectUser;
+        var selectedIndex = -1;
+
+        function addUser(){
+            var user = {
+                "fullName": $scope.fullName,
+                "email": "",
+                "username": $scope.username,
+                "password": "",
+                "phoneNumber": 0,
+                "aboutMe": "",
+                "interestedInvestments": "",
+                "roles": [$scope.role]
+            };
+            UserService.createUser(user,function(response){
+                console.log(response);
+            })
+        }
+
+        function updateUser(){
+
+        }
+
+        function deleteUser(){
+
+        }
+
+        function selectUser(){
+
+        }
     }
 }) ();
