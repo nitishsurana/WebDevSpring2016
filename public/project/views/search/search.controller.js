fc8b9fb5de51ffdbf11af5907479e3f63553234a
nitishsurana <surana.n@husky.neu.edu>
2016-03-11 15:26:22 -0500

    Added search bar to search results page.

--- public/project/views/search/search.controller.js
+++ public/project/views/search/search.controller.js
@@ -6,17 +6,36 @@
         .module("PortfolioManager")
         .controller("SearchController", SearchController);
 
-    function SearchController($rootScope,$routeParams){
+    function SearchController($scope, $location, $rootScope,$routeParams, SearchService){
+
+        $scope.search = search;
         var sym = $routeParams.symbol;
+
         console.log($rootScope.returnData);
         if (sym){
-            search(sym);
+            searchResult(sym);
         }
 
-        function search(s){
+        function searchResult(s){
             console.log(s);
             $location.url("/search/" + s);
+        }
 
+        function search(){
+            SearchService.search($scope.queryType, $scope.queryText);
+            $scope.$location.url('/search');
+
+            sym = $routeParams.symbol;
+
+            console.log($rootScope.returnData);
+            if (sym){
+                searchResult(sym);
+            }
+
+            function searchResult(s){
+                console.log(s);
+                $location.url("/search/" + s);
+            }
         }
     }
 })();
