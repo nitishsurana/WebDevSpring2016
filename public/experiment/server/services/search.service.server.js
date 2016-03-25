a1b3c92c6ede7a6123c14e2ae0bc7da5f081d04d
nitishsurana <surana.n@husky.neu.edu>
2016-03-24 20:12:53 -0400

    Added search user service on server.

--- public/experiment/server/services/search.service.server.js
+++ public/experiment/server/services/search.service.server.js
@@ -7,7 +7,7 @@ var http = require('http');
 module.exports = function(app, userModel){
     app.get("/api/project/search-option/:query", searchOption);
     app.get("/api/project/search-yahoo/:symbol", searchYahoo);
-    app.get("/api/project/search-investor", searchInvestor);
+    app.get("/api/project/search-investor/:query", searchInvestor);
 
     function searchOption(req, res){
         //console.log(req);
@@ -51,39 +51,17 @@ module.exports = function(app, userModel){
                 //console.log(d);
             });
             response.on('end', function() {
-
-                // Data reception is done, do whatever with it!
-                //console.log("end");
                 var parsed = JSON.parse(body);
                 //console.log(parsed);
                 res.send(parsed);
             });
 
-        });/*
-        http.get(request).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            if (response.data.query.results.quote.Name == ""){
-                $rootScope.returnData = {
-                    "Result": "Oops, the symbol does not exist",
-                    "symbol": symbol,
-                    "resultType": "YQL"
-                };
-
-            }
-            else {
-                res.send(response);
-            }
-            $location.url('/search');
-
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-            $location.url('/');
-            alert("Error in connection. Please try again.");
-        });*/
+        });
     }
-    function searchInvestor(){
-
+    
+    function searchInvestor(req, res){
+        var query = req.params.query;
+        var result = userModel.findUserByUsername(query);
+        res.send(result);
     }
-}
+};
