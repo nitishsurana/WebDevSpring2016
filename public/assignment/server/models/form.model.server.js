3bf6efafb3820c93e1367436c04018b6b2a0fd9d
nitishsurana <surana.n@husky.neu.edu>
2016-04-01 08:53:07 -0400

    Updated Form Model Server with Mongoose.

--- public/assignment/server/models/form.model.server.js
+++ public/assignment/server/models/form.model.server.js
@@ -5,10 +5,10 @@
 
 module.exports = function (db, mongoose) {
 
-    var forms = require("./form.mock.json");
-    var uuid = require('node-uuid');
+    //var forms = require("./form.mock.json");
+    //var uuid = require('node-uuid');
     var FormSchema = require('./form.schema.server.js')(mongoose);
-    var FieldSchema = require('./field.schema.server.js')(mongoose);
+    //var FieldSchema = require('./field.schema.server.js')(mongoose);
     var FormModel = mongoose.model('Form', FormSchema);
 
     var api = {
@@ -23,53 +23,100 @@ module.exports = function (db, mongoose) {
 
 
 
-    function createFormForUser(userId, title){
-        var new_form ={
-            "_id": uuid.v1(),
+    function createFormForUser(userId,title) {
+        var new_form = {
+            "userId": userId,
             "title": title,
-            "userId": userId
+            "fields": []
         };
+        var deferred = q.defer();
+        FormModel.create(new_form, function (err, doc) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(doc);
+            }
+        });
+        return deferred.promise();
+    }
+/*
         forms.push(new_form);
         console.log(new_form);
         return forms;
-    }
+  */
 
     function findFormById(formId){
+        var deferred = q.defer();
+        FormModel.findOne({_id: formId},function (err, doc) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(doc);
+            }
+        });
+        return deferred.promise();/*
         var result = [];
         for (var i=0; i<forms.length; i++){
             if (forms[i]._id === formId){
                 result.push(forms[i]);
             }
         }
-        return result;
+        return result;*/
     }
     function findAllFormsForUser(userId){
+        var deferred = q.defer();
+        FormModel.find({userId: userId},function (err, doc) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(doc);
+            }
+        });
+        return deferred.promise();/*
         var result = [];
         for (var i=0; i<forms.length; i++){
             if (forms[i].userId == userId){
                 result.push(forms[i]);
             }
         }
-        return result;
+        return result;*/
     }
 
     function deleteFormById(formId){
+        var deferred = q.defer();
+        FormModel.remove({_id: formId},function (err, doc) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(doc);
+            }
+        });
+        return deferred.promise();/*
         for (var i = 0; i<forms.length; i++){
             if (forms[i]._id == formId){
                 break;
             }
         }
         forms.splice(i,1);
-        return forms;
+        return forms;*/
     }
 
     function updateFormById(formId, newForm){
+        var deferred = q.defer();
+        FormModel.findOneAndUpdate({_id: formId},newForm, function (err, doc) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(doc);
+            }
+        });
+        return deferred.promise();/*
         for(var i = 0; i<forms.length; i++){
             if (forms[i]._id == formId){
                 forms[i].title = newForm;
                 break;
             }
         }
-        return forms;
+        return forms;*/
     }
-}
+};
