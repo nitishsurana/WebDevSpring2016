6cc9db3b3df93769f6b9ca04efb567e5521925c2
nitishsurana <surana.n@husky.neu.edu>
2016-04-01 21:19:28 -0400

    Updated server.js with Openshift variables for MongoDB.

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ var connectionString = 'mongodb://127.0.0.1:27017/formMaker';
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
 
